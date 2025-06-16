dc084fb9bb17be0e7afe9c9421c03d8bd486b6f0
panko <nathancarrasco07@gmail.com>
2025-06-16 13:46:55 -0500

    incorporacion del estilo.css

--- ValidarEjemploModificado.php
+++ ValidarEjemploModificado.php
@@ -3,13 +3,14 @@
 <html>
 <head>
     <title>Validaciones</title>
+    <link rel="stylesheet" href="estilos.css">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script> <!-- reCAPTCHA -->
 </head>
 <body>
-
+<div class="form-container">
 <h2>Ejemplos de Validaciones</h2>
 <form id="contactForm" method="post" action="validate2.php">
-    Name: <input type="text" name="name"><br><br>
+    Nombre: <input type="text" name="name"><br><br>
     Email: <input type="text" name="email"><br><br>
     Teléfono: <input type="text" name="phone" placeholder="123-456-7890"><br><br>
 
@@ -17,13 +18,13 @@
     <div class="g-recaptcha" data-sitekey="6LfBTmIrAAAAANDwMUbb59us-7mgvFoPLpbENTqK"></div>
     <br>
 
-    <input type="submit" name="submit" value="Submit">
+    <input type="submit" name="submit" value="Enviar">
 </form>
 
 <div id="errors" style="color:red; margin-top: 10px;"></div>
 <div id="response"></div>
 
 <script src="form-handler.js"></script>
-
+</div>
 </body>
 </html>
